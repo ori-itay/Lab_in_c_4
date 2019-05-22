bbbd2f26be553579ee44db96f1f9795e896d52ea
itay levi <eitay.levi@gmail.com>
2019-05-23 00:32:05 +0300

    last commit for today

--- src/ToyCAD.java
+++ src/ToyCAD.java
@@ -4,6 +4,7 @@ public class ToyCAD {
 	public static void main(String[] args) {
 		IShape shapes[];
 		System.out.println("showing git from cmd");
+		System.out.println("showing git from cmd");
 	}
 
 }
