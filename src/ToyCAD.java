e22b88fa581286e31ccb89c653d757baf80adfab
itay levi <eitay.levi@gmail.com>
2019-05-28 19:46:05 +0300

    Constructors rearranged + some functions implementations

--- src/ToyCAD.java
+++ src/ToyCAD.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
@@ -20,21 +21,35 @@ public class ToyCAD {
 		scanner.close();
 	}
 	
+	enum Method {
+		AREA, CIRCUMFERENCE
+	}
+	
 	private static boolean executeCommand(Map<String,Object> parsedCommand, List<IShape> shapes ) {
 		switch ((String)parsedCommand.get("command")) {
 			
-			case "new":
+			case "NEW":
 				addNewInstanceToShapes(shapes, parsedCommand); 
 				return false;
-			case "delete":
+			case "DELETE":
 				shapes.remove(parsedCommand.get("ID"));
 				return false;
-			case "move":
+			case "MOVE":
 				shapes.get((int) parsedCommand.get("ID")).move((double) parsedCommand.get("moveX"),(double) parsedCommand.get("moveY")); // now in whatever shape it is, use what's needed (for example if the shape if circle, in the implementation of move in circle it will use only x1 x2 and radius..(so need to change 'move' declaration)
 				return false;
-			case "copy": //specifically debug this
-				
-			case "is_inside":
+			case "COPY": //specifically debug this
+				shapes.get((int) parsedCommand.get("ID")).copy().move((double) parsedCommand.get("moveX"),(double) parsedCommand.get("moveY"));
+				return false;
+			case "AREA": 
+				printTotalColored(shapes,(IShape.Color) parsedCommand.get("color"), Method.AREA);
+				return false;
+			case "COLOR":
+				shapes.get((int) parsedCommand.get("ID")).setColor((IShape.Color) parsedCommand.get("color"));
+				return false;
+			case "CIRCUMFERENCE":
+				printTotalColored(shapes,(IShape.Color) parsedCommand.get("color"), Method.CIRCUMFERENCE);
+				return false;
+			case "IS_INSIDE":
 				if (shapes.get((int) parsedCommand.get("ID")).is_inside((double) parsedCommand.get("x"), (double) parsedCommand.get("y"))) { System.out.println("1");}
 				else {System.out.println("0");}
 				return false;
@@ -55,31 +70,31 @@ public class ToyCAD {
 		double y1 = (double) parsedCommand.get("y1");
 		IShape.Color color = (IShape.Color) parsedCommand.get("color");
 		switch (className) { // change to if? and then no code replication.... with x2 x3 etc
-			case "Ellipse":
+			case "ELLIPSE":
 				double D = (double) parsedCommand.get("D");
 				IShape ellipse = new Ellipse(color, x1, y1, (double) parsedCommand.get("x2"), (double) parsedCommand.get("y2"), D);
 				//ellipse.calculateArea();
 				shapes.add(ellipse); // debug that its the incremented ID..
 				break;
-			case "Circle":
+			case "CIRCLE":
 				double radius = (double) parsedCommand.get("radius");
 				IShape circle = new Circle(color, x1, y1, radius);
 				shapes.add(circle);
 				break;
-			case "Parallelogram":
+			case "PARALLELOGRAM":
 				IShape parallelogram = new Parallelogram(color, x1, y1, (double) parsedCommand.get("x2"), (double) parsedCommand.get("y2"), (double) parsedCommand.get("x3"), (double) parsedCommand.get("y3"));
 				shapes.add(parallelogram);
 				break;
-			case "Rectangle":
+			case "RECTANGLE":
 				IShape rectangle = new Rectangle(color, x1, y1, (double) parsedCommand.get("x2"), (double) parsedCommand.get("y2"));
 				shapes.add(rectangle);
 				break;
-			case "Square":
+			case "SQUARE":
 				double length = (double) parsedCommand.get("length");
 				IShape square = new Square(color, x1, y1, length) ;
 				shapes.add(square);
 				break;
-			case "Triangle":
+			case "TRIANGLE":
 				IShape triangle = new Triangle(color, x1, y1, (double) parsedCommand.get("x2"), (double) parsedCommand.get("y2"), (double) parsedCommand.get("x3"), (double) parsedCommand.get("y3"));
 				shapes.add(triangle);
 				break;
@@ -91,32 +106,33 @@ public class ToyCAD {
 
 	private static Map<String, Object> parseLine(String line) {
 		Map<String, Object> parsedCommand = new HashMap<>();
-		List<String> splittedLine = Arrays.asList(line.split(" "));
+		String Uppercased_line = line.toUpperCase();
+		List<String> splittedLine = Arrays.asList(Uppercased_line.split(" "));
 		if (splittedLine.get(0).equals("")) { splittedLine.remove(0); }
 		parsedCommand.put("command", splittedLine.get(0));
 		
 		switch (splittedLine.get(0)) {
-			case ("new"):
+			case ("NEW"):
 				parseNewCommand(splittedLine, parsedCommand);
 				break;
-			case ("delete"):
+			case ("DELETE"):
 				parsedCommand.put("ID",    Integer.valueOf(splittedLine.get(1)));
 				break;
-			case ("move"): // upper case or lower case?. case move and copy are the same, it will add the same arguments to the parsedCommand 
-			case ("copy"):
+			case ("MOVE"): // case move and copy are the same, it will add the same arguments to the parsedCommand 
+			case ("COPY"):
 				parsedCommand.put("ID",    Integer.valueOf(splittedLine.get(1)));
 				parsedCommand.put("moveX", Double.valueOf(splittedLine.get(2)));
 				parsedCommand.put("moveY", Double.valueOf(splittedLine.get(3)));
 				break;
-			case ("area"):
-			case ("circumference"):
+			case ("AREA"):
+			case ("CIRCUMFERENCE"):
 				parsedCommand.put("color", IShape.Color.valueOf(splittedLine.get(1)));
 				break;
-			case ("color"): // upper case or lower case?
+			case ("COLOR"): 
 				parsedCommand.put("color", IShape.Color.valueOf(splittedLine.get(1)));
 				parsedCommand.put("ID", Integer.valueOf(splittedLine.get(2)));
 				break;
-			case ("is_inside"):
+			case ("IS_INSIDE"):
 				parsedCommand.put("ID", Integer.valueOf(splittedLine.get(1)));
 				parsedCommand.put("x", Double.valueOf(splittedLine.get(2)));
 				parsedCommand.put("y", Double.valueOf(splittedLine.get(3)));
@@ -136,30 +152,43 @@ public class ToyCAD {
 		parsedCommand.put("x1", Double.valueOf(splittedLine.get(3)));
 		parsedCommand.put("y1", Double.valueOf(splittedLine.get(4)));
 		
-		if (splittedLine.get(1).equals("Ellipse") ||
-				splittedLine.get(1).equals("Parallelogram") || 
-				splittedLine.get(1).equals("Rectangle") || 
-				splittedLine.get(1).equals("Triangle")) 
+		if (splittedLine.get(1).equals("ELLIPSE") ||
+				splittedLine.get(1).equals("PARALLELOGRAM") || 
+				splittedLine.get(1).equals("RECTANGLE") || 
+				splittedLine.get(1).equals("TRIANGLE")) 
 		{
 			parsedCommand.put("x2", Double.valueOf(splittedLine.get(5)));
 			parsedCommand.put("y2", Double.valueOf(splittedLine.get(6)));
 		}
-		if (splittedLine.get(1).equals("Paralellogram") || splittedLine.get(1).equals("Triangle")) {
+		if (splittedLine.get(1).equals("PARALLELOGRAM") || splittedLine.get(1).equals("TRIANGLE")) {
 			parsedCommand.put("x3", Double.valueOf(splittedLine.get(7)));
 			parsedCommand.put("y3", Double.valueOf(splittedLine.get(8)));
 		}
 		
-		if (splittedLine.get(1).equals("Circle")) {
+		if (splittedLine.get(1).equals("CIRCLE")) {
 			parsedCommand.put("radius", Double.valueOf(splittedLine.get(5)));
 		}
-		else if (splittedLine.get(1).equals("Ellipse")) {
+		else if (splittedLine.get(1).equals("ELLIPSE")) {
 			parsedCommand.put("D", Double.valueOf(splittedLine.get(7)));
 		}
-		else if (splittedLine.get(1).equals("Square")) {
+		else if (splittedLine.get(1).equals("SQUARE")) {
 			parsedCommand.put("length", Double.valueOf(splittedLine.get(5)));
 		}
 	}
 	
-	
-
+	static void printTotalColored(List<IShape> shapes, IShape.Color color, Method method) {
+		DecimalFormat formatter = new DecimalFormat("#0.00");
+		double totalColored = 0;
+		
+		for (IShape shape : shapes) {
+			if(method == Method.AREA && shape.getColor() == color) {
+				totalColored+= shape.getArea();
+			}
+			else if(method == Method.CIRCUMFERENCE && shape.getColor() == color) {
+				totalColored+= shape.getCircumference();
+			}
+		}
+		System.out.println(formatter.format(totalColored));
+		return;
+	}
 }
